d6e6446c4e656763ff00cfab75c309e6f4e91b8a
= <=>
2020-02-17 18:24:18 +0000

    Rollup scaffolding with shortterm MANIFEST library cache file

--- tool/rollup/config.js
+++ tool/rollup/config.js
@@ -3,23 +3,51 @@ import path from "path"
 import includePaths from "rollup-plugin-includepaths"
 import multiEntry from "@rollup/plugin-multi-entry"
 
+// TODO consider `process.chdir("../../")` instead
 const ROOT_DIR = path.resolve(__dirname, `../../`)
 const OUT_DIR = path.resolve(ROOT_DIR, `build`)
 const SRC_DIR = path.resolve(ROOT_DIR, `src/main/js`)
 
 export default {
+
+  /* @see https://rollupjs.org/guide/en/#input */
   input: {
-    include: `${SRC_DIR}/**/*.js`
+    include: [
+      /*
+       * Path to the custom non-executable library MANIFEST file
+       * It is a simple linking file that details the libraries
+       * exports and nothing more.
+       * @see Java MANIFEST
+       * @see HTML5 CACHE MANIFEST
+       */
+      `${ROOT_DIR}/tool/rollup/MANIFEST`
+
+      /*
+       * Glob defining paths to legitimate source files
+       */
+    , `${SRC_DIR}/**/*.js`
+    ]
   },
+
+  /* @see https://rollupjs.org/guide/en/#output */
   output: {
     file: `${OUT_DIR}/scriptex-${pkg.version}.js`,
-    format: "esm"
+    format: "esm",
+    /* @see https://rollupjs.org/guide/en/#outputinterop */
+    interop: false,
+    plugins: [ /* @todo terser */ ]
   },
+
+  /* @see https://rollupjs.org/guide/en/#plugins */
   plugins: [
+
+    /* @see https://github.com/dot-build/rollup-plugin-includepaths */
     includePaths({
-      paths: [SRC_DIR]
-    })
-  , multiEntry({
+      paths: [ SRC_DIR ]
+    }),
+
+    /* @see https://github.com/rollup/plugins/tree/master/packages/multi-entry */
+    multiEntry({
       exports: true
     })
   ]
