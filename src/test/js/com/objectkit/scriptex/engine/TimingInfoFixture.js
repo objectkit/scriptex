de5c742b265b72eeb88ac077c99f7f88757c0e4b
David Foley <david@objectkit.com>
2020-02-26 18:19:28 +0000

    Added TimingInfoFixture interface property placeholders

--- src/test/js/com/objectkit/scriptex/engine/TimingInfoFixture.js
+++ src/test/js/com/objectkit/scriptex/engine/TimingInfoFixture.js
@@ -1,3 +1,21 @@
+/* @interface */
 export default class TimingInfoFixture {
-  
+ /* @type {boolean} */
+ get playing () {}
+ /* @type {number} */
+ get blockStartBeat () {}
+ /* @type {number} */
+ get blockEndBeat () {}
+ /* @type {number} */
+ get tempo () {}
+ /* @type {number} */
+ get meterNumerator () {}
+ /* @type {number} */
+ get meterDenominator () {}
+  /* @type {boolean} */
+ get cycling () {}
+ /* @type {number} */
+ get leftCycleBeat () {}
+ /* @type {number} */
+ get rightCycleBeat () {}
 }
