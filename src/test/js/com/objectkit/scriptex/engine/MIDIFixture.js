002976e0e589d636ef459cd53d475928d44a9326
David Foley <david@objectkit.com>
2020-02-26 18:18:44 +0000

    Added MIDI interface method placeholders

--- src/test/js/com/objectkit/scriptex/engine/MIDIFixture.js
+++ src/test/js/com/objectkit/scriptex/engine/MIDIFixture.js
@@ -1,4 +1,16 @@
 export default class MIDIFixture {
 
+  noteNumber (noteName) {}
+  noteName (noteNumber) {}
+  ccName (ccNumber) {}
+  normalizeStatus (statusNumber) {}
+  normalizeChannel (channelNumber) {}
+  normalizeData (number) {}
   allNotesOff () {}
+
+  /* @private
+  _sendEventOnAllChannels(midiEvent) {}
+  get _ccNames () {}
+  get _noteNames () {}
+  */
 }
