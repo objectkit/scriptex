ac6dfa5c125e8b3c5e55ac7ee0db1ad4e435fd47
David Foley <david@objectkit.com>
2020-02-26 18:55:43 +0000

    Add PluginFixture for integration testing

--- src/test/js/com/objectkit/scriptex/plugin/PluginFixture.js
+++ src/test/js/com/objectkit/scriptex/plugin/PluginFixture.js
@@ -11,56 +11,120 @@ export default class PluginFixture extends Plugin {
   }
 
   get parameters () {
+    let minVelocity = {
+      ID: "minVelocity"
+    , name: "Minimum Velocity"
+    , type: "lin"
+    , minValue: 0
+    , maxValue: 127
+    , defaultValue: 0
+    , numberOfSteps: 127
+    }
+
+    let maxVelocity = {
+      ID: "maxVelocity"
+    , name: "Maximum Velocity"
+    , type: "lin"
+    , minValue: 0
+    , maxValue: 127
+    , defaultValue: 127
+    , numberOfSteps: 127
+    }
+
+    let midiPanic = {
+      ID: "midiPanic"
+    , name: "PANIC"
+    , type: "momentary"
+    }
+
+    let loggingEnabled = {
+      ID: "loggingEnabled"
+    , name: "Log MIDI"
+    , type: "checkbox"
+    , defaultValue: 0
+    }
+
+    let processEnabled = {
+      ID: "processing"
+    , name: "Processing"
+    , type: "checkbox"
+    , defaultValue: 1
+    }
+
     return [
-      {
-        ID: "muted"
-      , type: "checkbox"
-      },
-      {
-        ID: "note"
-      , type: "menu"
-      , valueStrings: [
-          "C", "E", "G"
-        ]
-      , defaultValue: 0
-      , readOnly: false
-      , hidden: false
-      },
-      {
-        ID: "midiPanic"
-      , type: "momentary"
-      }
+      processEnabled
+    , midiPanic
+    , loggingEnabled
+    , minVelocity
+    , maxVelocity
     ]
   }
 
-  /* @see handleParameter */
   set midiPanic (pressed) {
     this.engine.MIDI.allNotesOff()
   }
 
-  handleMIDI (midi) {
-    if (this.muted)
-      return
-    this.engine.SendMIDIEventNow(midi)
+  set minVelocity (val) {
+    if (val > this._maxVelocity) {
+      this.engine.SetParameter("Minimum Velocity", this._maxVelocity - 1)
+    }
+    else {
+      this._minVelocity = val
+    }
   }
 
-  handleProcess () {
-    this.hostInfo = this.engine.GetTimingInfo()
+  set maxVelocity (val) {
+    if (val < this._minVelocity) {
+      this.engine.SetParameter("Maximum Velocity", this._minVelocity + 1)
+    }
+    else {
+      this._maxVelocity = val
+    }
+  }
+
+  handleMIDI (midi) {
+    if (!(this.processing)) {
+      return
+    }
+
+    if (midi.hasOwnProperty("velocity")) {
+      let velocity = midi.velocity
+      if (velocity > this._maxVelocity)
+        velocity = this._maxVelocity
+      if (velocity < this._minVelocity)
+        velocity = this._minVelocity
+      midi.velocity = velocity
+    }
+
+    this.engine.SendMIDIEventNow(midi)
+
+    if (this.loggingEnabled) {
+      this.engine.Trace(midi)
+    }
   }
 
   handleParameter (index, data) {
-    let model = this
-    let param = this.parameters[index]
-    let key = param.ID
-    model[key] = data
-    return
+  	this[this.parameters[index].ID] = data
   }
 
   handleReset () {
-    this.engine.Trace(`Reset`)
+    this.engine.Trace("RESET")
   }
 
+  /*
+    NOTE
+    When ResetParameterDefaults is true and UpdatePluginParameters is called,
+    Then all parameters are reset to their default values.
+   */
   handleIdle () {
-    this.engine.UpdatePluginParameters()
+    if (!(this.resetParameters)){
+      this.engine.UpdatePluginParameters()
+    }
+  }
+
+  handleProcess () {
+    if (this.needsTiming){
+      this.hostState = this.engine.GetTimingInfo()
+    }
   }
 }
