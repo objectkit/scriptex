890215accc42e7e28a94f8420d4a20cbe45d0964
David Foley <david@objectkit.com>
2020-07-17 16:46:01 +0100

    Simplified Plugin.deploy impl.

--- src/main/js/com/objectkit/scriptex/plugin/Plugin.js
+++ src/main/js/com/objectkit/scriptex/plugin/Plugin.js
@@ -1,16 +1,9 @@
 import Scriptex from "com/objectkit/scriptex/Scriptex"
-import Scripter from "com/objectkit/scriptex/engine/Scripter"
 
 class Plugin {
-  static get API () {
-    return Scriptex.API
+  static deploy (...rest) {
+    return new Scriptex(...rest).deploy(new this())
   }
-
-  /* NOTE purposeful arrangement of parameters - configurable option is runtime option */
-  static deploy (configurable, api=this.API, engine) {
-    return new Scriptex(configurable, api, engine).deploy(new this())
-  }
-
 }
 
 export default Plugin
