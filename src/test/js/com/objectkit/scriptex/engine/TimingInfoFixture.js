a6c4f740d8073681f47e161d83cb765386bc4801
David Foley <david@objectkit.com>
2020-07-22 22:21:12 +0100

    TiminInfoFixture now has setter/getter property pairs

--- src/test/js/com/objectkit/scriptex/engine/TimingInfoFixture.js
+++ src/test/js/com/objectkit/scriptex/engine/TimingInfoFixture.js
@@ -1,29 +1,76 @@
 /* @interface */
-export default class TimingInfoFixture {
- /* @type {boolean} */
- get playing () {
-   return true
- }
- /* @type {number} */
- get blockStartBeat () {}
- /* @type {number} */
- get blockEndBeat () {}
- /* @type {number} */
- get tempo () {
-   return 120
- }
- /* @type {number} */
- get meterNumerator () {
-   return 4
- }
- /* @type {number} */
- get meterDenominator () {
-   return 4
- }
+class TimingInfoFixture {
+
+  constructor () {
+    this.playing = true
+    this.tempo = 120
+    this.meterNumerator = 4
+    this.meterDenominator = 4
+  }
+
   /* @type {boolean} */
- get cycling () {}
- /* @type {number} */
- get leftCycleBeat () {}
- /* @type {number} */
- get rightCycleBeat () {}
+  get playing() {
+    return this.$playing
+  }
+  set playing (any) {
+    this.$playing = any
+  }
+  /* @type {number} */
+  get blockStartBeat() {
+    return this.$blockStartBeat
+  }
+  set blockStartBeat(any) {
+    this.$blockStartBeat = any
+  }
+  /* @type {number} */
+  get blockEndBeat() {
+    return this.$blockEndBeat
+  }
+  set blockEndBeat(any){
+    this.$blockEndBeat = any
+  }
+  /* @type {number} */
+  get tempo() {
+    return this.$tempo
+  }
+  set tempo (any) {
+    this.$tempo = any
+  }
+  /* @type {number} */
+  get meterNumerator() {
+    return this.$meterNumerator
+  }
+  set meterNumerator(any) {
+    return this.$meterNumerator = any
+  }
+  /* @type {number} */
+  get meterDenominator() {
+    return this.$meterDenominator
+  }
+  set meterDenominator(any) {
+    this.$meterDenominator = any
+  }
+  /* @type {boolean} */
+  get cycling() {
+    return this.$cycling
+  }
+  set cycling(any) {
+    this.$cycling = any
+  }
+  /* @type {number} */
+  get leftCycleBeat() {
+    return this.$leftCycleBeat
+  }
+  set leftCycleBeat(any) {
+    this.$leftCycleBeat = any
+  }
+  /* @type {number} */
+  get rightCycleBeat() {
+    return this.$rightCycleBeat
+  }
+  set rightCycleBeat(any) {
+    this.$rightCycleBeat = any
+  }
 }
+
+export default TimingInfoFixture
