1a8b99e4e08e22c804b40f0057904ebe614f4916
David Foley <objectkit@users.noreply.github.com>
2020-08-24 21:35:34 +0100

    Tidy

--- src/main/js/com/objectkit/scriptex/plugin/Plugin.js
+++ src/main/js/com/objectkit/scriptex/plugin/Plugin.js
@@ -1,13 +1,12 @@
 import Scriptex from "com/objectkit/scriptex/Scriptex"
 
 class Plugin {
-
+  
   static get API () {
     return Scriptex.API
   }
 
   static deploy (engine, configurable, ...ctorArgs) {
-    // return new Scriptex(engine, this.API, configurable).deploy(new this(...rest))
     let deployee = Reflect.construct(this, ctorArgs)
     let deployer = new Scriptex(engine, this.API, configurable)
     return deployer.deploy(deployee)
