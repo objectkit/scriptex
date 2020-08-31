9e5d8137be6fa934a8c26f381a996166c1f435ef
David Foley <objectkit@users.noreply.github.com>
2020-08-31 12:22:57 +0100

    DOC: improved Scripter documentation

--- src/main/js/com/objectkit/scriptex/engine/Scripter.js
+++ src/main/js/com/objectkit/scriptex/engine/Scripter.js
@@ -1,5 +1,7 @@
 /**
- * [Scripter description]
+ * A reference to Scripter, which in terms of the Scripter deployment environment,
+ * equates simply to the global scope.
+ *
  * @type {Object}
  * @see {@link https://manuals.info.apple.com/MANUALS/1000/MA1925/en_US/logic-pro-x-10-5-effects-user-guide.pdf}
  */
