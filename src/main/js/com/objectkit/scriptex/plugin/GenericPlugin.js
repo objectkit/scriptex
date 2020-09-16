4786d8a81cf61c0b459be503e947a6ea59f6df24
David Foley <objectkit@users.noreply.github.com>
2020-09-16 01:21:22 +0100

    [DROP] deprecated imports

--- src/main/js/com/objectkit/scriptex/plugin/GenericPlugin.js
+++ src/main/js/com/objectkit/scriptex/plugin/GenericPlugin.js
@@ -1,71 +1,177 @@
 import Plugin from "com/objectkit/scriptex/plugin/Plugin"
 
+/**
+ * @classdesc
+ * The GenericPlugin class is a minimal implementation useful for quick debelopment and testing.
+ * The implementation of onParam essentialy treats the Plugin as a "ParamModelView", while
+ * the implementation of onMidi delegates midi events to dedicated handlers.
+ *
+ * @extends Plugin
+ */
 class GenericPlugin extends Plugin {
 
+  /**
+   * Manage calls to Scripter.ParameterChanged
+   *
+   * @param  {number} key The index of a param
+   * @param  {number} val The new value of the param
+   * @return {void}
+   */
   onParam (key, val) {
     (key = this.params[key].ID) && (this[key] = val)
   }
 
+  /**
+   * Manage calls to Scripter.HandleMIDI
+   *
+   * @param  {Event} midi A Scripter MIDI event
+   * @return {number}     The beatPos that the midi event was sent at
+   * @see [onNoteOn]{@link GenericPlugin#onNoteOn}
+   * @see [onNoteOff]{@link GenericPlugin#onNoteOn}
+   * @see [onControlChange]{@link GenericPlugin#onControlChange}
+   * @see [onProgramChange]{@link GenericPlugin#onProgramChange}
+   * @see [onTargetEvent]{@link GenericPlugin#onTargetEvent}
+   * @see [onPitchBend]{@link GenericPlugin#onPitchBend}
+   * @see [onEvent]{@link GenericPlugin#onEvent}
+   */
   onMidi (midi) {
-    // return this["on" + this.getEventName(midi)](midi)
-    let eventName = this.getEventName(midi)
-    let methodKey = "on" + eventName
-    return this[methodKey](midi)
+    return this[("on"+this.getEventName(midi))](midi)
   }
 
+  /**
+   * Manage a NoteOn event.
+   *
+   * @param  {NoteOn} event A NoteOn event
+   * @return {number}       The beatPos that the event was sent at
+   * @see [onMidi]{@link GenericPlugin#onMidi}
+   * @see [onNote]{@link GenericPlugin#onNote}
+   */
   onNoteOn (event) {
     return this.onNote(event)
   }
 
+
+  /**
+   * Manage a NoteOff event.
+   *
+   * @param  {NoteOff} event A NoteOff event
+   * @return {number}       The beatPos that the event was sent at
+   * @see [onMidi]{@link GenericPlugin#onMidi}
+   * @see [onNote]{@link GenericPlugin#onNote}
+   */
   onNoteOff (event) {
     return this.onNote(event)
   }
 
+  /**
+   * Manage a NoteOn or NoteOff event.
+   *
+   * @param  {NoteOff|NoteOn} event A NoteOff or NoteOn event
+   * @return {number}       The beatPos that the event was sent at
+   * @see [onNoteOn]{@link GenericPlugin#onNote}
+   * @see [onNoteOff]{@link GenericPlugin#onNote}
+   * @see [sendMidi]{@link GenericPlugin#sendMidi}
+   */
   onNote (event) {
     return this.sendMidi(event)
   }
 
+  /**
+   * Manage a ControlChange event.
+   *
+   * @param  {ControlChange} event A ControlChange event
+   * @return {number}       The beatPos that the event was sent at
+   * @see [onMidi]{@link GenericPlugin#onMidi}
+   * @see [sendMidi]{@link GenericPlugin#sendMidi}
+   */
   onControlChange (event) {
     return this.sendMidi(event)
   }
 
+  /**
+   * Manage a ProgramChange event.
+   *
+   * @param  {ProgramChange} event  A ProgramChange event
+   * @return {number}               The beatPos that the event was sent at
+   * @see [onMidi]{@link GenericPlugin#onMidi}
+   * @see [sendMidi]{@link GenericPlugin#sendMidi}
+   */
   onProgramChange (event) {
     return this.sendMidi(event)
   }
 
+  /**
+   * Manage a PolyPressure event.
+   *
+   * @param  {PolyPressure} event   A PolyPressure event
+   * @return {number}              The beatPos that the event was sent at
+   * @see [onMidi]{@link GenericPlugin#onMidi}
+   * @see [sendMidi]{@link GenericPlugin#sendMidi}
+   */
   onPolyPressure (event) {
     return this.sendMidi(event)
   }
 
+  /**
+   * Manage a ChannelPressure event.
+   *
+   * @param  {ChannelPressure} event   A ChannelPressure event
+   * @return {number}              The beatPos that the event was sent at
+   * @see [onMidi]{@link GenericPlugin#onMidi}
+   * @see [sendMidi]{@link GenericPlugin#sendMidi}
+   */
   onChannelPressure (event) {
     return this.sendMidi(event)
   }
 
+  /**
+   * Manage a TargetEvent event.
+   *
+   * @param  {TargetEvent} event   A TargetEvent event
+   * @return {number}              The beatPos that the event was sent at
+   * @see [onMidi]{@link GenericPlugin#onMidi}
+   * @see [sendMidi]{@link GenericPlugin#sendMidi}
+   */
   onTargetEvent (event) {
     return this.sendMidi(event)
   }
 
+  /**
+   * Manage a PitchBend event.
+   *
+   * @param  {PitchBend} event    A TargetEvent event
+   * @return {number}             The beatPos that the event was sent at
+   * @see [onMidi]{@link GenericPlugin#onMidi}
+   * @see [sendMidi]{@link GenericPlugin#sendMidi}
+   */
   onPitchBend (event) {
     return this.sendMidi(event)
   }
 
+  /**
+   * Manage a generic Event instance.
+   *
+   * @param  {PitchBend} event    A generic Event instance
+   * @return {number}             The beatPos that the event was sent at
+   * @see [onMidi]{@link GenericPlugin#onMidi}
+   * @see [sendMidi]{@link GenericPlugin#sendMidi}
+   */
   onEvent (event) {
     return this.sendMidi(event)
   }
 
-  sendMidi (event) {
-    let beatPos = event.beatPos || 0
-
-    /* @todo sendAtBeat should be priority */
-    0 > beatPos
-      ? event.sendAfterBeats(beatPos *= -1)
-      : !Number.isFinite(beatPos)
-        ? event.sendAfterMilliseconds(beatPos = +Math.abs(beatPos))
-        : event.sendAtBeat(beatPos)
-
-    return beatPos
-  }
-
+  /**
+   * Determine the name of a Scripter MIDI event.
+   *
+   * NOTE: in earlier editions of Logic Pro X, determining the name of an
+   * event was as simple as checking the event constructors name
+   * ``Trace(event.constructor.name) // e.g. "NoteOn"``
+   * This method compares the midi status code to a known list of event name correlates
+   * to accomodate.
+   *
+   * @param  {Event} event Any Scripter MIDI event
+   * @return {string}      The name of the MIDI event
+   */
   getEventName (event) {
     let status = event.status
     switch(status) {
@@ -82,6 +188,35 @@ class GenericPlugin extends Plugin {
         throw new ReferenceError(`EventNameNotFound: ${status}`)
       }
     }
+  }
+
+  /**
+   * Send a Scripter MIDI event using any one of the four avalable midi dispatch methods
+   *
+   * When beatPos is missing
+   *  Then event.send is used
+   * When beatPos is a postive nunber
+   *  Then event.sendAtBeat is used
+   * When beatPos is a negative number
+   *  Then event.sendAfterBeats is used with beatPos flipped to positive number
+   * When beatPos is a string (postive or negative)
+   *  Then event.sendAfterMilliseconds is used with beatPos cast to positive number
+   *
+   * @param  {Event} event Any supported Scripter MIDI Event instance.
+   * @return {number}      The beatPos at which the event was sent.
+   */
+  sendMidi (event) {
+    let beatPos = event.beatPos || 0
+
+    /** @todo send needs support for cases when NeedsTimingInfo is false */
+    /** @todo sendAtBeat should be priority */
+    0 > beatPos
+      ? event.sendAfterBeats(beatPos *= -1)
+      : !Number.isFinite(beatPos)
+        ? event.sendAfterMilliseconds(beatPos = +Math.abs(beatPos))
+        : event.sendAtBeat(beatPos)
+
+    return beatPos
   }
 }
 
--- tool/rollup/index.js
+++ tool/rollup/index.js
@@ -2,8 +2,4 @@
 export {default as Scriptex} from "com/objectkit/scriptex/Scriptex"
 export {default as Scripter} from "com/objectkit/scriptex/engine/Scripter"
 export {default as Plugin} from "com/objectkit/scriptex/plugin/Plugin"
-/** @deprecated */
-export {default as PluginTemplate} from "com/objectkit/scriptex/plugin/PluginTemplate"
-/** @deprecated */
-export {default as EventNames} from "com/objectkit/scriptex/util/midi/EventNames"
 export {default as GenericPlugin} from "com/objectkit/scriptex/plugin/GenericPlugin"
