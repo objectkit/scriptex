407e6d91cf3958c965cc26dcfd8bc7bf5c19bfad
David Foley <objectkit@users.noreply.github.com>
2020-10-16 13:52:20 +0100

    test(ScripterSpec): chai.assert integration

--- src/test/js/com/objectkit/scriptex/system/ScripterSpec.js
+++ src/test/js/com/objectkit/scriptex/system/ScripterSpec.js
@@ -1,6 +1,9 @@
+import chai from "chai"
 import { Scripter } from "@objectkit/scriptex"
+
 describe(`Scripter`, () => {
   specify(`Scripter is a reference to the global scope`, () => {
+    const { assert } = chai
     assert.strictEqual(Scripter, global)
   })
 })
