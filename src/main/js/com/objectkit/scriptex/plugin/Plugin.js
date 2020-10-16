13fa5f121afd2f554a9d5e233d7f8775b7e8137c
David Foley <objectkit@users.noreply.github.com>
2020-10-16 13:44:34 +0100

    feat(Plugin): CONFIGURABLE doc(Plugin): #system, #init

--- src/main/js/com/objectkit/scriptex/plugin/Plugin.js
+++ src/main/js/com/objectkit/scriptex/plugin/Plugin.js
@@ -4,6 +4,31 @@ import { Scriptex } from "com/objectkit/scriptex/Scriptex"
 class Plugin {
 
   /**
+   * Plugins may add an optional static CONFIGURABLE property to influence
+   * the configurablility of system properties created by Scriptex during
+   * the deployment process. Its primary benefit is for debugging the
+   * Scripter environment itself rather than the plugin instance.
+   *
+   * @example
+   *  class SystemDebugPlugin extends Plugin {
+   *    //... Scriptex api hooks here
+   *  }
+   *
+   *  // instruct Scriptex to make Scripter properties configurable
+   *  SystemDebugPlugin.CONFIGURABLE= true
+   *  SystemDebugPlugin.deploy()
+   *  // debug Scripter here
+   *  // e.g Reflect.deleteProperty(Scripter, `PluginParameters`)
+   *
+   * As this is to handle transient edge cases, this capacity is not
+   * formally implemented here.
+   * @static
+   * @abstract
+   * @see [Plugin.deploy]{@link Plugin.deploy}
+   */
+
+
+  /**
    * The Scripter/Scriptex interface that the Plugin will conform to.
    * The default interface is Scriptex.API.
    * Subclasses can return a list of their own bindings for custom integrations
@@ -55,8 +80,6 @@ class Plugin {
   /**
    * Instantiate and integrate a new plugin with the Scripter environment.
    *
-   * @param  {Object}  [system=Scripter]
-   * @param  {Boolean} [configurable=false]
    * @param  {...*}  [ctorArgs=[]]
    *  Any arguments to pass to Plugin subclass constructors.
    * @return {Array<string>}
@@ -65,11 +88,24 @@ class Plugin {
    */
   static deploy (...ctorArgs) {
     const deployee = new this(...ctorArgs)
-    const deployer = new Scriptex(Scriptex.SYSTEM, this.API, deployee.configurable)
+    const deployer = new Scriptex(Scriptex.SYSTEM, this.API, this.CONFIGURABLE)
     return deployer.deploy(deployee)
   }
 
+  /**
+   * Set the plugins system context. This is undertaken by Scriptex during the
+   * course of deployment, but can also be of use when unit testing bespoke
+   * midi processors.
+   *
+   * Note that accessing #system before it is set will result in a
+   * "SystemAccessFault" being thrown.
+   *
+   * @type {Scripter|Object}
+   * @see {@link #init()}
+   * @see {@link Plugin.deploy()}
+   */
   set system (system) {
+    /* @final property */
     Reflect.defineProperty(this, `system`, { value: system })
     this.init()
   }
@@ -78,6 +114,15 @@ class Plugin {
     throw new ReferenceError(`SystemAccessFault`)
   }
 
+  /**
+   * An initialistion method invoked after #system has been
+   * set by Scriptex. Subclasses can override this and define
+   * the plugins configuration after construction but before
+   * deployment.
+   * @abstract
+   * @return {void}
+   * @see {@link #system}
+   */
   init () {}
 }
 
