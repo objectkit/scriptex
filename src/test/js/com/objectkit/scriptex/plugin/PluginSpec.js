d9f83be83bc47f74162ef6ed647777aa36e1bc87
David Foley <objectkit@users.noreply.github.com>
2020-10-16 13:47:03 +0100

    test(Plugin): - .deploy - .API - #init - #system

--- src/test/js/com/objectkit/scriptex/plugin/PluginSpec.js
+++ src/test/js/com/objectkit/scriptex/plugin/PluginSpec.js
@@ -1,17 +1,114 @@
-import { Scriptex, Plugin } from "@objectkit/scriptex"
+import sinon from "sinon"
+import chai from "chai"
+import { Scriptex, Plugin, Scripter } from "@objectkit/scriptex"
 
-const sandbox = sinon.createSandbox()
 describe(`Plugin`, () => {
-  
-  beforeEach(()=>{})
 
-  afterEach(()=>{})
+  const { assert } = chai
+  const sandbox= sinon.createSandbox()
 
-  describe(`static API:Map<string,string>`, () =>{ })
+  afterEach(()=>{
+    sandbox.restore()
+  })
 
-  describe(`static deploy(...ctorArgs):Array<string>`, () => { })
+  describe(`static API:Map<string,string>`, () =>{
+    context(`Given Plugin.API is accessed`, () => {
+      specify(`Then Scriptex.API is returned [default]`, () => {
+        const scriptexApiSpy= sandbox.spy(Scriptex, `API`, [`get`]).get
+        let apiMap= null
+        sandbox.assert.notCalled(scriptexApiSpy)
+        {
+          apiMap= Plugin.API // access
+        }
+        sandbox.assert.calledOnce(scriptexApiSpy)
+        assert.deepEqual(Plugin.API, Scriptex.API)
+      })
+    })
+  })
 
-  describe(`#init():void`, () => { })
+  /** @todo Plugin.API use case demonstration test */
+  describe(`static deploy(...ctorArgs):Array<string>`, () => {
+    context(`Given parameters supplied to Plugin.deploy`, () => {
+      class ConstructorArgumentsCapturingPlugin extends Plugin {
+        /* constructor cannot be spied on */
+        constructor (...ctorArgs) {
+          super()
+          this.ctorArgs= ctorArgs
+        }
+      }
+      specify(`Then supplied parameters are given to the Plugin constructor`, () => {
+        const ctorArgs= [1, 2, 3]
+        const deploySpy= sandbox.spy(Scriptex.prototype, `deploy`)
+        {
+          ConstructorArgumentsCapturingPlugin.deploy( ...ctorArgs )
+        }
+        assert.deepEqual((deploySpy.lastCall.firstArg).ctorArgs, ctorArgs)
+      })
+    })
+    /* edge case */
+    context(`Given Plugin has a static "CONFIGURABLE" property`, () => {
 
-  describe(`#system`, () => {})
+      beforeEach(() => {
+        /* setup */
+        sandbox.stub(Scriptex, `SYSTEM`).get( () => Object.create(null) )
+        sandbox.spy(Scriptex.prototype, `deploy`)
+      })
+
+      class ConfigurableSystemPlugin extends Plugin {
+        /* hook for testing */
+        onParam () {}
+      }
+      const { isTrue, isFalse } = assert
+      const assertSystemIsConfigurable= (configurable, assertion) => {
+        ConfigurableSystemPlugin.CONFIGURABLE = configurable
+        {
+          ConfigurableSystemPlugin.deploy()
+        }
+        const { system } = Scriptex.prototype.deploy.lastCall.firstArg
+        const { configurable: val } = Reflect.getOwnPropertyDescriptor(system, `ParameterChanged`)
+        assertion( val )
+      }
+
+      describe(`When "configurable" is true`, () => {
+        specify(`Then #system properties are configurable`, () => {
+          assertSystemIsConfigurable(true, isTrue)
+        })
+      })
+      describe(`When "configurable" is false`, () => {
+        specify(`Then #system properties are non-configurable`, () => {
+          assertSystemIsConfigurable(false, isFalse)
+        })
+      })
+    })
+  })
+  describe(`#init():void`, () => {
+    afterEach(() => sandbox.restore())
+    context(`When #system is set`, () => {
+      specify(`Then #init() is automatically invoked`, () => {
+        const plugin= sandbox.spy(new Plugin())
+        sandbox.assert.notCalled(plugin.init)
+        {
+          plugin.system= {}
+        }
+        sandbox.assert.calledOnce(plugin.init)
+      })
+    })
+  })
+  describe(`#system`, () => {
+    context(`Given #system is accessed before Scriptex#deploy`, () => {
+      specify(`Then a "SystemAccessFault" advisory is thrown`, () => {
+        assert.throws(() => {new Plugin().system}, ReferenceError, "SystemAccessFault" )
+      })
+    })
+    context(`Given #system is accessed after Plugin.deploy`, () => {
+      specify(`Then a reference to Scripter is returned.`, () => {
+        const deploySpy= sandbox.spy(Scriptex.prototype, `deploy`)
+        {
+          Plugin.deploy()
+        }
+        const plugin = deploySpy.lastCall.firstArg
+        assert.propertyVal(plugin, `system`, Scripter)
+      })
+    })
+  })
 })
